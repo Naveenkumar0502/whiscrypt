8f07ae8826ea5bf06fb2bb22fb76d632093276c2
Naveenkumar0502 <navin882295@gmail.com>
2025-03-31 16:38:44 +0530

    auth and authorisation is completed waiting for env and db.js please check muku and niti.

---
+++ Backend/src/controllers/authController.js
@@ -0,0 +1,31 @@
+const User = require('../models/User');
+const { generateToken } = require('../utils/jwt');
+
+exports.register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        let user = await User.findOne({ email });
+        if (user) return res.status(400).json({ message: 'User already exists' });
+
+        user = new User({ username, email, password });
+        await user.save();
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const token = generateToken(user);
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
+    }
+};
